9511d40198463e91920be5e08a2f0cc22d8b3920
SimonjShurety <simonjshurety@gmail.com>
2023-08-28 12:11:17 -0300

    Move tasks to README.md

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # c_plus_plus_projects
+
+
+
+function to get the sum of an array (hint use accumulator)
+
+function for average
+
+function that gives max value of the array
+
+function that gives min value of array
+
+make and use a 2d array
+
+print function for 2d array
+
+etc...
+
+make sure to comment
--- arrayintroduction.cpp
+++ arrayintroduction.cpp
@@ -54,22 +54,3 @@ int main()
     // 1,2,3,4,
     return 0;
 }
-
-/*
-
-function to get the sum of an array (hint use accumulator)
-
-function for average
-
-function that gives max value of the array
-
-function that gives min value of array
-
-make and use a 2d array
-
-print function for 2d array
-
-etc...
-
-make sure to comment
-*/
